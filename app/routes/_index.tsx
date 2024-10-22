37ddc41940fbd64d5065d8f9543a8316d1b8061e
Malik Bagwala <m.bagwala@gmail.com>
2024-10-22 14:28:51 +0530

    chore: make changes to the layout

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,20 +9,22 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="h-screen w-full flex justify-center items-center bg-teal-100 text-teal-900 font-bold">
-      <article className="prose">
-        <h1>Garlic bread with cheese: What the science tells us</h1>
-        <p>
-          For years parents have espoused the health benefits of eating garlic
-          bread with cheese to their children, with the food earning such an
-          iconic status in our culture that kids will often dress up as warm,
-          cheesy loaf for Halloween.
-        </p>
-        <p>
-          But a recent study shows that the celebrated appetizer may be linked
-          to a series of rabies cases springing up around the country.
-        </p>
-      </article>
+    <div className="h-screen w-full bg-teal-100 text-teal-900 font-bold">
+      <div className="container mx-auto flex justify-center items-center h-full">
+        <article className="prose">
+          <h1>Garlic bread with cheese: What the science tells us</h1>
+          <p>
+            For years parents have espoused the health benefits of eating garlic
+            bread with cheese to their children, with the food earning such an
+            iconic status in our culture that kids will often dress up as warm,
+            cheesy loaf for Halloween.
+          </p>
+          <p>
+            But a recent study shows that the celebrated appetizer may be linked
+            to a series of rabies cases springing up around the country.
+          </p>
+        </article>
+      </div>
     </div>
   );
 }
