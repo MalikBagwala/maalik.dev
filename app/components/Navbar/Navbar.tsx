2ede8f35b1ea7744a55cfc4443bc23604d61e119
Malik Bagwala <m.bagwala@gmail.com>
2024-10-24 15:36:39 +0530

    chore: navbar change

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from "@remix-run/react";
 const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 w-full bg-white/70 backdrop-blur-lg backdrop-saturate-150 border-gray-200 border-b">
-      <div className="container py-4">
+      <div className="container py-6">
         <div className="flex text-xl">
           <div className="flex flex-shrink-0 gap-2 items-center font-semibold mr-auto">
             <img src="/logo.webp" height={30} width={30} alt="Logo" />
