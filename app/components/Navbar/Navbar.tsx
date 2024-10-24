3eb81fc8f6fb060fe194ba617989cb3d8935cf3f
Malik Bagwala <m.bagwala@gmail.com>
2024-10-24 15:21:21 +0530

    chore: add core navbar

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
-    <nav className="container py-4">
-      <div>
+    <nav className="fixed top-0 left-0 w-full bg-white/70 backdrop-blur-lg backdrop-saturate-150 border-gray-200 border-b">
+      <div className="container py-4">
         <div className="flex flex-shrink-0 gap-2 items-center font-semibold">
           <img src="/logo.webp" height={30} width={30} alt="Logo" />
           <h1>maalik.dev</h1>
