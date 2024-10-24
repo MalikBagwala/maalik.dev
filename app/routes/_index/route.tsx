51658bbf76b8f2bbda50b4823414c271bda3faa7
Malik Bagwala <m.bagwala@gmail.com>
2024-10-24 15:32:43 +0530

    chore: tweak the layouts

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -7,21 +7,26 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const TestSection = () => {
+  return (
+    <div className="container grid grid-cols-3 gap-8">
+      <img src="https://picsum.photos/600/600" alt="" />
+      <img src="https://picsum.photos/600/601" alt="" />
+      <img src="https://picsum.photos/600/602" alt="" />
+      <img src="https://picsum.photos/600/604" alt="" />
+      <img src="https://picsum.photos/600/606" alt="" />
+      <img src="https://picsum.photos/600/609" alt="" />
+      <img src="https://picsum.photos/600/610" alt="" />
+      <img src="https://picsum.photos/600/611" alt="" />
+      <img src="https://picsum.photos/600/612" alt="" />
+      <img src="https://picsum.photos/600/613" alt="" />
+    </div>
+  );
+};
 export default function Index() {
   return (
-    <div className="min-h-screen w-full bg-gray-100 text-teal-900 font-bold pt-[calc(63px+2rem)]">
-      <div className="container grid grid-cols-3 gap-8">
-        <img src="https://picsum.photos/600/600" alt="" />
-        <img src="https://picsum.photos/600/601" alt="" />
-        <img src="https://picsum.photos/600/602" alt="" />
-        <img src="https://picsum.photos/600/604" alt="" />
-        <img src="https://picsum.photos/600/606" alt="" />
-        <img src="https://picsum.photos/600/609" alt="" />
-        <img src="https://picsum.photos/600/610" alt="" />
-        <img src="https://picsum.photos/600/611" alt="" />
-        <img src="https://picsum.photos/600/612" alt="" />
-        <img src="https://picsum.photos/600/613" alt="" />
-      </div>
+    <div className="bg-gray-100 content">
+      <TestSection />
     </div>
   );
 }
