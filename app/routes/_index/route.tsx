85723293aa52ec87c1f6bd482925dbf90a03b79a
Malik Bagwala <m.bagwala@gmail.com>
2024-11-04 15:25:13 +0530

    chore: add styling changes

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -5,7 +5,6 @@ import Hero from "./Hero";
 import Projects from "./Projects";
 import Skills from "./Skills";
 import Testimonials from "./Testimonials";
-
 export const meta: MetaFunction = () => {
   return [
     { title: "Malik Bagwala | Freelance Software Engineer" },
@@ -17,22 +16,6 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export const TestSection = () => {
-  return (
-    <div className="container grid grid-cols-3 gap-8">
-      <img src="https://picsum.photos/600/600" alt="" />
-      <img src="https://picsum.photos/600/601" alt="" />
-      <img src="https://picsum.photos/600/602" alt="" />
-      <img src="https://picsum.photos/600/604" alt="" />
-      <img src="https://picsum.photos/600/606" alt="" />
-      <img src="https://picsum.photos/600/609" alt="" />
-      <img src="https://picsum.photos/600/610" alt="" />
-      <img src="https://picsum.photos/600/611" alt="" />
-      <img src="https://picsum.photos/600/612" alt="" />
-      <img src="https://picsum.photos/600/613" alt="" />
-    </div>
-  );
-};
 export default function Index() {
   return (
     <>
