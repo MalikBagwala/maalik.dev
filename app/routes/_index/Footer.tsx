413d37958971a3dd71da86c16af3fefca6a41894
Malik Bagwala <m.bagwala@gmail.com>
2024-11-05 15:03:46 +0530

    chore: cool footer hover effect

--- app/routes/_index/Footer.tsx
+++ app/routes/_index/Footer.tsx
@@ -1,13 +1,10 @@
-import { animated, useSpring, useTrail } from "@react-spring/web";
-import { useState } from "react";
-import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
 import Section from "~/components/Section/Section";
 import Github from "~/icons/Github";
 import GitLab from "~/icons/GitLab";
 import Linkedin from "~/icons/Linkedin";
 import Twitter from "~/icons/Twitter";
 import Upwork from "~/icons/Upwork";
-
 export const SOCIALS = [
   {
     icon: <Linkedin height={22} className="social" />,
@@ -33,90 +30,64 @@ export const SOCIALS = [
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const AnimatedSocialIcon = ({ children, link }: any) => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  const springProps = useSpring({
-    transform: isHovered ? "rotate(360deg)" : "rotate(0deg)",
-    config: { tension: 300, friction: 10 },
-  });
-
   return (
-    <animated.a
+    <motion.a
       href={link}
       target="__blank"
-      style={springProps}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
+      initial={{ rotate: "0deg" }}
+      whileHover={{ rotate: "360deg", scale: 1.5 }}
       className="transition-colors hover:fill-blue-500"
     >
       {children}
-    </animated.a>
+    </motion.a>
   );
 };
 
 const Footer = () => {
-  const { ref, inView } = useInView({
-    triggerOnce: true,
-    threshold: 0.1,
-  });
-
-  const trail = useTrail(3, {
-    opacity: inView ? 1 : 0,
-    y: inView ? 0 : 20,
-    from: { opacity: 0, y: 20 },
-    config: { mass: 1, tension: 200, friction: 20 },
-  });
-
-  const socialTrail = useTrail(SOCIALS.length, {
-    opacity: inView ? 1 : 0,
-    y: inView ? 0 : 20,
-    from: { opacity: 0, y: 20 },
-    config: { mass: 1, tension: 200, friction: 20 },
-    delay: 200,
-  });
-
   return (
-    <Section
-      id="footer"
-      mainProps={{
-        className:
-          "flex flex-col gap-8 lg:gap-0 text-center lg:flex-row justify-center lg:justify-between items-center lg:items-start",
-      }}
-    >
-      <animated.div
-        className="flex flex-col text-gray-500"
-        style={trail[0]}
-        ref={ref}
-      >
-        <a className="text-blue-500" href="https://maalik.dev" target="__blank">
-          Maalik
-        </a>
-        <p>
-          Jamali Residency
-          <br /> Nashik, India
-          <br />
-          422011
-        </p>
-      </animated.div>
+    <Section id="footer">
+      <motion.div className="flex flex-col gap-8 lg:gap-0 text-center lg:flex-row justify-center lg:justify-between items-center lg:items-start">
+        <motion.div className="flex flex-col text-gray-500">
+          <a
+            className="text-blue-500"
+            href="https://maalik.dev"
+            target="__blank"
+          >
+            Maalik
+          </a>
+          <p>
+            Jamali Residency
+            <br /> Nashik, India
+            <br />
+            422011
+          </p>
+        </motion.div>
 
-      <animated.div className="flex flex-col text-gray-500" style={trail[1]}>
-        <a className="text-blue-500" href="https://maalik.dev" target="__blank">
-          Terms & Conditions
-        </a>
-        <p>© {new Date().getFullYear()} Malik Bagwala</p>
-      </animated.div>
+        <motion.div className="flex flex-col text-gray-500">
+          <a
+            className="text-blue-500"
+            href="https://maalik.dev"
+            target="__blank"
+          >
+            Terms & Conditions
+          </a>
+          <p>© {new Date().getFullYear()} Malik Bagwala</p>
+        </motion.div>
 
-      <animated.div className="flex flex-col text-gray-500" style={trail[2]}>
-        <div className="flex gap-4 fill-blue-300 items-center">
-          {SOCIALS.map((social, index) => (
-            <animated.div key={social.link} style={socialTrail[index]}>
-              <AnimatedSocialIcon link={social.link}>
+        <motion.div className="flex flex-col text-gray-500">
+          <motion.div
+            initial={{ gap: "16px" }}
+            whileHover={{ gap: "20px" }}
+            className="flex fill-blue-300 items-center"
+          >
+            {SOCIALS.map((social) => (
+              <AnimatedSocialIcon link={social.link} key={social.link}>
                 {social.icon}
               </AnimatedSocialIcon>
-            </animated.div>
-          ))}
-        </div>
-      </animated.div>
+            ))}
+          </motion.div>
+        </motion.div>
+      </motion.div>
     </Section>
   );
 };
