7c338d28aa3fa1deb398f9c38666da20b1143be1
Malik Bagwala <m.bagwala@gmail.com>
2024-11-13 14:29:43 +0530

    chore: fix the mobile UI

--- app/components/Project/Project.tsx
+++ app/components/Project/Project.tsx
@@ -34,38 +34,36 @@ const Project = (project: ProjectProps) => {
       className="rounded-lg border mb-10 last:mb-0 shadow-lg text-gray-700 bg-white"
     >
       <Link
-        className="h-96 relative cursor-pointer"
+        className="h-96 cursor-pointer"
         preventScrollReset
         to={{ pathname: `/p/${slug}` }}
         state={project}
       >
-        <motion.img
-          loading="lazy"
-          src={thumbnail}
-          alt={`${title} thumbnail`}
-          className="w-full h-full object-cover rounded-t-lg"
-        />
-        <div className="absolute top-0 left-0 w-full p-4 flex flex-col justify-between h-full">
-          <div className="flex justify-between">
-            <a
-              onClick={(e) => e.stopPropagation()}
-              href={liveLink}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="uppercase text-xl lg:text-2xl font-bold tracking-wider text-white backdrop-blur-md backdrop-saturate-100 backdrop-brightness-[0.7] hover:backdrop-brightness-[0.4] px-2 rounded-full transition duration-200 transform hover:scale-105"
-            >
-              {title}
-            </a>
-            <a
-              onClick={(e) => e.stopPropagation()}
-              href={sourceCodeLink}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-white backdrop-blur-md backdrop-saturate-100 backdrop-brightness-[0.8] rounded-full flex justify-center items-center min-w-[32px] transition duration-200 transform hover:rotate-180"
-            >
-              <Github fill="white" height={24} />
-            </a>
-          </div>
+        <div className="relative">
+          <motion.img
+            loading="lazy"
+            src={thumbnail}
+            alt={`${title} thumbnail`}
+            className="w-full h-full object-cover rounded-t-lg"
+          />
+          <a
+            onClick={(e) => e.stopPropagation()}
+            href={liveLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="absolute top-4 left-4 uppercase text-xl lg:text-2xl font-bold tracking-wider text-white backdrop-blur-md backdrop-saturate-100 backdrop-brightness-[0.7] hover:backdrop-brightness-[0.4] px-2 rounded-full transition duration-200 transform hover:scale-105"
+          >
+            {title}
+          </a>
+          <a
+            onClick={(e) => e.stopPropagation()}
+            href={sourceCodeLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="absolute top-4 right-4 text-white backdrop-blur-md backdrop-saturate-100 backdrop-brightness-[0.8] rounded-full flex justify-center items-center min-w-[32px] transition duration-200 transform hover:rotate-180 p-1"
+          >
+            <Github fill="white" height={24} />
+          </a>
         </div>
       </Link>
       <div className="p-4">
